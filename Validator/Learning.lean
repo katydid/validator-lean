361cb680660bae0d11802141c027caca6c893d73
Walter Schulze <awalterschulze@gmail.com>
2025-07-21 14:09:34 +0100

    temporarily add capturing for regular expressions

--- Validator/Learning.lean
+++ Validator/Learning.lean
@@ -6,3 +6,6 @@ import Validator.Learning.ImperativeLeave
 import Validator.Learning.Memoize
 import Validator.Learning.Original
 import Validator.Learning.Parser
+
+import Validator.Learning.Regex.Regex
+import Validator.Learning.Regex.RegexCapture
